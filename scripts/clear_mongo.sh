0f22e07539bf15978f903def6d897465822b7321
stevenchiu30801 <steven30801@gmail.com>
2020-03-10 15:29:08 +0800

    drop policyData.ues.amData collection in clear mongo script

--- scripts/clear_mongo.sh
+++ scripts/clear_mongo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Drop collections created by free5GC Stage 2 in Mongo DB
 
-declare -a collectionlist=("NfProfile" "UriList" "subscriptionData.authenticationData.authenticationStatus" "subscriptionData.authenticationData.authenticationSubscription" "subscriptionData.contextData.amf3gppAccess" "subscriptionData.contextData.sdmSubscriptions" "subscriptionData.provisionedData.amData" "subscriptionData.provisionedData.smfSelectionSubscriptionData")
+declare -a collectionlist=("NfProfile" "UriList" "policyData.ues.amData" "subscriptionData.authenticationData.authenticationStatus" "subscriptionData.authenticationData.authenticationSubscription" "subscriptionData.contextData.amf3gppAccess" "subscriptionData.contextData.sdmSubscriptions" "subscriptionData.provisionedData.amData" "subscriptionData.provisionedData.smfSelectionSubscriptionData")
 
 MONGO_POD=$( kubectl get pod -l app.kubernetes.io/instance=mongo -l app.kubernetes.io/name=mongo -o jsonpath='{.items[0].metadata.name}' )
 

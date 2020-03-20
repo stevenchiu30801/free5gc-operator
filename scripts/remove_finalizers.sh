39e6d0a7267625a5e89fa4a0835dce91fbf4cf87
stevenchiu30801 <steven30801@gmail.com>
2020-03-20 14:24:39 +0800

    add remove_finalizers script

---
+++ scripts/remove_finalizers.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Remove finalizers in Free5GCSlice CRs
+
+CRS=$( kubectl get free5gcslice.bans.io -o jsonpath='{.items[?(@.metadata.finalizers)].metadata.name}' )
+IFS=' ' read -r -a cr_list <<< "$CRS"
+
+for cr in "${cr_list}"
+do
+    kubectl get free5gcslice.bans.io $cr -o=json | jq '.metadata.finalizers = null' | kubectl apply -f -
+done
